b23e1f6caa64ab8cad3d030d47350305bf84ae29
troy-hamilton <121830956+troy-hamilton@users.noreply.github.com>
2023-01-16 21:07:12 -0600

    Add files via upload

--- macig1/step_out.cpp
+++ macig1/step_out.cpp
@@ -11,10 +11,11 @@ int putdata(int client)
 /*this is the forking version of step out
 reads and writes are done sequentially.
 if a future version of this program uses
-non blocking io, then it would probably check for conditions,
-then start some kind of non blocking read or write, then return,
-and be re-called when the read or write completes, but for now it is
-a shell allowing portability.
+non blocking io, then it would probably read in data first, then pass it
+to this function, then call further(),check for conditions,
+then start some kind of non blocking read or write (unless the condition is WRITEFUL)
+, then return, and be re-called when the read or write completes, but for now it is
+a shell allowing portability with future versions.
 */
 
 void step_out(int client)
@@ -30,7 +31,14 @@ void step_out(int client)
 	
 	
 	retval = further(client, request); // refer to readme to build
-	struct http_table httptable; // memset this
-	retval = process_data(request, httptable);
+	if(retval == conditions)
+	{
+		...
+	}
+	else if(...)
+	{
+		...
+	}
+	...
 	
 }
