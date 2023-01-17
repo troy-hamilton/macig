40ded671b31e8ef424128dd253e71381740f746a
troy-hamilton <121830956+troy-hamilton@users.noreply.github.com>
2023-01-16 21:13:00 -0600

    Update main.cpp

--- macig1/main.cpp
+++ macig1/main.cpp
@@ -30,6 +30,6 @@ int looper()
 }
 
 int main()
-{
+{// fix step out so it ignores a -1 in the case that looper fails
 	step_out(looper()); // main() will loop on looper. step_out() will only be called in the forked process
 }
